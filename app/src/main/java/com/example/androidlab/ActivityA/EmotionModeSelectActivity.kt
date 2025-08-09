9851d767e964025d993331a1b65911f351cb32ba
junbeom1111 <abm1111@naver.com>
2025-08-09 11:26:37 +0900

    픽셀 화면 크기조정

--- app/src/main/java/com/example/androidlab/ActivityA/EmotionModeSelectActivity.kt
+++ app/src/main/java/com/example/androidlab/ActivityA/EmotionModeSelectActivity.kt
@@ -2,6 +2,7 @@ package com.example.androidlab.ActivityA
 // EmotionModeSelectActivity.kt
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidlab.R
@@ -12,8 +13,8 @@ class EmotionModeSelectActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_emotion_mode_select)
 
-        val practiceButton = findViewById<Button>(R.id.button_practice)
-        val testButton = findViewById<Button>(R.id.button_test)
+        val practiceButton: View = findViewById(R.id.button_practice)
+        val testButton: View = findViewById(R.id.button_test)
 
         practiceButton.setOnClickListener {
             val intent = Intent(this, EmotionPracticeActivity::class.java)
